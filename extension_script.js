64707cbc1f0b894ab1cdeab687263f4fded5f8e4
thupvm <minhthu.phanvu@gmail.com>
2021-05-05 22:17:20 +0700

    Tooltip

--- extension_script.js
+++ extension_script.js
@@ -1,25 +1,104 @@
 //JAVASCRIPT DOCUMENT
+// Tooltip
+const tooltipObjectEl = document.getElementsByClassName('tooltip')[0]
+tooltipObjectEl && tooltipObjectEl.addEventListener('click', handleShowTooltip)
+// tooltipObjectEl && tooltipObjectEl.addEventListener('mouseout', handleHideTooltip)
+
+const popupEl = document.createElement("div");
+popupEl.classList.add('popup')
+
+function handleShowTooltip() {
+    const tooltipPosition = this.getAttribute('data-position')
+    const tooltipContent = this.getAttribute('data-content')
+    // cut string
+    const subStringContentArray = tooltipPosition.split(' ')
+    popupEl.innerHTML = tooltipContent
+    popupEl.classList.add(subStringContentArray[0])
+    popupEl.classList.add(subStringContentArray[1])
+
+    const objectPostion = this.getBoundingClientRect()
+    const topObj = objectPostion.top
+    const leftObj = objectPostion.left
+    const bottomObj = objectPostion.bottom
+    const rightObj = objectPostion.right
+
+    document.body.appendChild(popupEl)
+
+    if (subStringContentArray[1] === 'left') {
+        popupEl.style.left = leftObj + 'px'
+    }
+    if (subStringContentArray[1] === 'right') {
+        popupEl.style.left = (rightObj - popupEl.offsetWidth) + 'px'
+    }
+    if (subStringContentArray[1] === 'center') {
+        popupEl.style.left = (leftObj + (this.offsetWidth -  popupEl.offsetWidth)/2) + 'px'
+    }
+    if (subStringContentArray[0] === 'top') {
+        popupEl.style.top = (topObj - popupEl.offsetHeight - 10) + 'px'
+    }
+    if (subStringContentArray[0] === 'bottom') {
+        popupEl.style.top = (topObj + this.offsetHeight) + 'px'
+    }
+    if (subStringContentArray[0] === 'right') {
+        popupEl.style.top = (topObj + (this.offsetHeight - popupEl.offsetHeight)/2 ) + 'px'
+        popupEl.style.left = rightObj + 'px'
+    }   
+    if (subStringContentArray[0] === 'left') {
+        popupEl.style.top = (topObj + (this.offsetHeight - popupEl.offsetHeight)/2 ) + 'px'
+        popupEl.style.left = (leftObj - popupEl.offsetWidth) + 'px'
+    }
+}
+function handleHideTooltip() {
+    popupEl.remove()
+}
+
+
+
+// click vao .dropdown-wrap
+const dropdownEl = document.getElementsByClassName('dropdown-wrap')[0]
+const menuEl = dropdownEl && dropdownEl.children[2]
+const menuItems = menuEl && menuEl.children || []
+const textEl = dropdownEl && dropdownEl.children[1]
+
+textEl && textEl.addEventListener('click', handleDropdown)
+// -> toggle .hidden cho .menu | toggle cho icon xoay xuong/len
+
+function handleDropdown() {
+    menuEl.classList.toggle('hidden')
+    dropdownEl.classList.toggle('dropdown-no-boder-bottom')
+}
+// click vao tung item: query all items -> get data-value, text cua current item -> gan vao .text
+//add .hidden vao .menu va remove class .transition cho icon
+for(let i = 0; i < menuItems.length; i++) {
+    menuItems[i] && menuItems[i].addEventListener('click', handleClickMenuItem)
+}
+function handleClickMenuItem(e) {
+    textEl.innerHTML = this.innerHTML
+    textEl.setAttribute('data-value', this.getAttribute('data-value'))
+    menuEl.classList.add('hidden')
+    dropdownEl.classList.remove('dropdown-no-boder-bottom')
+}
 
 // Handle Collapse
 // Get current class is using height
-const firstCardHeaderEl = document.getElementsByClassName('card-header')[0]
-const firstCardContentEl = firstCardHeaderEl.nextElementSibling
-const firstCardBodyEl = firstCardContentEl.children[0]
-firstCardContentEl.style.height = `${firstCardBodyEl.clientHeight}px`
+const firstHeaderEl = document.getElementsByClassName('header')[0]
+const firstBodyContentEl = firstHeaderEl && firstHeaderEl.nextElementSibling
+const firstContentEl = firstBodyContentEl && firstBodyContentEl.children[0]
+firstBodyContentEl && (firstBodyContentEl.style.height = `${firstContentEl.clientHeight}px`)
 
-const headerCollapseEl = document.getElementsByClassName('card-header')
-for(let i = 0; i < headerCollapseEl.length; i++ ) {
-    headerCollapseEl[i].addEventListener('click', handleCollapse)
+const headerEl = document.getElementsByClassName('header')
+for(let i = 0; i < headerEl.length; i++ ) {
+    headerEl[i].addEventListener('click', handleCollapse)
 }
 
 function handleCollapse() {
-    //add class show for current .collapse
-    const cardContentEl = this.nextElementSibling
-    if (cardContentEl.clientHeight) {
-        cardContentEl.style.height = 0;
+    //add .show for current .body-content
+    const bodyContentEl = this.nextElementSibling
+    if (bodyContentEl.clientHeight) {
+        bodyContentEl.style.height = 0;
     } else {
-        const cardBodyEl = cardContentEl.children[0]
-        cardContentEl.style.height = `${cardBodyEl.clientHeight}px`
+        const contentEl = bodyContentEl.children[0]
+        bodyContentEl.style.height = `${contentEl.clientHeight}px`
     }
 
     const siblingsEl = getSiblings(this)
@@ -28,7 +107,7 @@ function handleCollapse() {
     }
 }
 
-function getSiblings(e) { //e = .card-header
+function getSiblings(e) { //e = .header
     let siblings = []
     // if no parent, return no sibling
     if(!e.parentElement && !e.parentElement.parentElement) {
@@ -38,10 +117,10 @@ function getSiblings(e) { //e = .card-header
     let sibling  = e.parentElement.parentElement.children[0]; //.card 
 
     while(sibling) {
-        const cardHeaderEl = sibling.children[0]; //.card-header
-        if (cardHeaderEl !== e) {
-            const cardContent = sibling.children[1]; //.collapse la nextSibling cua .card-header
-            siblings.push(cardContent);
+        const headerEl = sibling.children[0]; //.header of sibling
+        if (headerEl !== e) {
+            const bodyContent = sibling.children[1]; //.body-content la nextSibling cua .header
+            siblings.push(bodyContent);
         }
         sibling = sibling.nextElementSibling;
     }
@@ -94,66 +173,5 @@ function handleOpenTabs() {
     for(let i = 0; i < siblingsTabPaneEl.length; i++) {
         siblingsTabPaneEl[i].classList.remove('show')
     }
-}
-
-// click vao .dropdown-wrap
-const dropdownEl = document.getElementsByClassName('dropdown-wrap')[0]
-const menuEl = dropdownEl.children[2]
-const menuItems = menuEl.children
-const textEl = dropdownEl.children[1]
-
-textEl.addEventListener('click', handleDropdown)
-// -> toggle .hidden cho .menu | toggle cho icon xoay xuong/len
-
-function handleDropdown() {
-    menuEl.classList.toggle('hidden')
-    dropdownEl.classList.toggle('dropdown-no-boder-bottom')
-}
-// click vao tung item: query all items -> get data-value, text cua current item -> gan vao .text
-//add .hidden vao .menu va remove class .transition cho icon
-for(let i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener('click', handleClickMenuItem)
-}
-function handleClickMenuItem(e) {
-    textEl.innerHTML = this.innerHTML
-    textEl.setAttribute('data-value', this.getAttribute('data-value'))
-    menuEl.classList.add('hidden')
-    dropdownEl.classList.remove('dropdown-no-boder-bottom')
-}
-
-// handleFormSubmit
-const btnSend = document.getElementsByClassName('btn-send')[0];
-btnSend.addEventListener('click', handleModalFormContact)
-
-function setValueForm (id) {
-    document.querySelectorAll(`[data-content=${id}]`)[0].innerHTML = document.getElementById(id).value
-} 
-
-function handleModalFormContact() {
-    const modalFormEl = document.getElementById('modalFormContact')
-    modalFormEl.classList.add('display-block');
-    setValueForm('nameInput');
-    setValueForm('emailInput');
-    setValueForm('phoneInput');
-    setValueForm('messageInput');
-    handleBackgroundModal();
-}
-
-const crossCloseEls = document.getElementsByClassName('btn-close');
-for(let i = 0; i < crossCloseEls.length; i++)
-{
-    crossCloseEls[i].addEventListener('click', handleCloseModal)
-}
-function handleCloseModal() {
-    const parentEl = this.parentElement
-    parentEl.classList.remove('display-block')
-    document.body.classList.remove('open-modal')
-    document.getElementsByClassName('modal-container')[0].remove()
-}
-function handleBackgroundModal() {
-    let modalContainerEl = document.createElement("DIV");   // Create a <div> element
-    modalContainerEl.classList.add('modal-container') // add class 'modal-continer' in css to DIV el
-    document.body.appendChild(modalContainerEl); //append modalContainerEl as the last node in body
-    document.body.classList.add('open-modal') // add open-modal class to body
 }
 
